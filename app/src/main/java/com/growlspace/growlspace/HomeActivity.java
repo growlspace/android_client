505371407ea0ea6c38e24636cf12d6991aac6a61
Nicholas Tinsley <00sturm@gmail.com>
2015-04-25 23:12:49 -0400

    added navigation drawer
    It's all pretty and stuff

--- app/src/main/java/com/growlspace/growlspace/HomeActivity.java
+++ app/src/main/java/com/growlspace/growlspace/HomeActivity.java
@@ -3,8 +3,16 @@ package com.growlspace.growlspace;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+import com.mikepenz.materialdrawer.Drawer;
+import com.mikepenz.materialdrawer.accountswitcher.AccountHeader;
+import com.mikepenz.materialdrawer.model.PrimaryDrawerItem;
+import com.mikepenz.materialdrawer.model.SecondaryDrawerItem;
 
 public class HomeActivity extends AppCompatActivity {
+    Drawer.Result result;
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +24,19 @@ public class HomeActivity extends AppCompatActivity {
                     .commit();
         }
         initActionBar();
+        AccountHeader.Result headerResult = new AccountHeader()
+                .withActivity(this)
+                .withHeaderBackground(R.drawable.abc_cab_background_top_material)
+                .build();
+        result = new Drawer()
+                .withActivity(this)
+                .withToolbar(toolbar)
+                .withAccountHeader(headerResult)
+                .addDrawerItems(
+                        new PrimaryDrawerItem().withName(R.string.feed),
+                        new SecondaryDrawerItem().withName(R.string.action_settings)
+                )
+                .build();
     }
 
     private void initActionBar() {
@@ -24,5 +45,6 @@ public class HomeActivity extends AppCompatActivity {
             actionBar.hide();
         }
     }
+
 
 }

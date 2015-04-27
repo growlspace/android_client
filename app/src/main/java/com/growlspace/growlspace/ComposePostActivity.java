5fe1d9f42158ad88d6d6d9febdb163d33748acfa
Nicholas Tinsley <00sturm@gmail.com>
2015-04-27 13:07:33 -0400

    removed unnecessary static variables
    DRY

--- app/src/main/java/com/growlspace/growlspace/ComposePostActivity.java
+++ app/src/main/java/com/growlspace/growlspace/ComposePostActivity.java
@@ -1,20 +1,17 @@
 package com.growlspace.growlspace;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
-import android.widget.Toast;
 
-import com.google.gson.JsonObject;
 import com.growlspace.growlspace.entity.Post;
 import com.growlspace.growlspace.media.AudioRecorder;
-import com.koushikdutta.async.future.FutureCallback;
-import com.koushikdutta.ion.Ion;
 
 import java.util.UUID;
 
@@ -46,24 +43,17 @@ public class ComposePostActivity extends AppCompatActivity {
         OnClickListener submitClicker = new OnClickListener() {
             @Override
             public void onClick(View v) {
-                String caption =  ((EditText) findViewById(R.id.postBodyEditText)).getText().toString();
+                String caption = ((EditText) findViewById(R.id.postBodyEditText)).getText().toString();
                 String audioFilePath = audioRecorder.getFileName();
-                Post post = new Post(audioFilePath, caption);
 
-                Ion.with(v.getContext())
-                .load("https://koush.clockworkmod.com/test/echo")
-                        .setJsonPojoBody(post)
-                        .asJsonObject()
-                        .setCallback(new FutureCallback<JsonObject>() {
-                            @Override
-                            public void onCompleted(Exception e, JsonObject result) {
-                                if (e != null) {
-                                    Log.e(LOG_TAG, e.toString());
-                                }
-                                Log.d(LOG_TAG, result.toString());
-                            }
-                        });
-                Toast.makeText(v.getContext(), "This will post this message one day.", Toast.LENGTH_SHORT).show();
+                Intent resultIntent = new Intent();
+                resultIntent.putExtra(Post.AUDIO_FILE_PATH, audioFilePath);
+                resultIntent.putExtra(Post.CAPTION, caption);
+
+                Activity parentActivity = (Activity) v.getContext();
+
+                parentActivity.setResult(Activity.RESULT_OK, resultIntent);
+                parentActivity.finish();
             }
         };
         submitButton.setOnClickListener(submitClicker);

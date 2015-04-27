b9f59606556a966b283b30046a3a93740e8dbcff
Nicholas Tinsley <00sturm@gmail.com>
2015-04-27 12:23:57 -0400

    first networking features
    currently just sends back an echo

--- app/src/main/java/com/growlspace/growlspace/ComposePostActivity.java
+++ app/src/main/java/com/growlspace/growlspace/ComposePostActivity.java
@@ -2,6 +2,7 @@ package com.growlspace.growlspace;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -9,13 +10,17 @@ import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.Toast;
 
+import com.google.gson.JsonObject;
 import com.growlspace.growlspace.entity.Post;
 import com.growlspace.growlspace.media.AudioRecorder;
+import com.koushikdutta.async.future.FutureCallback;
+import com.koushikdutta.ion.Ion;
 
 import java.util.UUID;
 
 
 public class ComposePostActivity extends AppCompatActivity {
+    private static final String LOG_TAG = "ComposeActivity";
 
     AudioRecorder audioRecorder = null;
 
@@ -44,6 +49,20 @@ public class ComposePostActivity extends AppCompatActivity {
                 String caption =  ((EditText) findViewById(R.id.postBodyEditText)).getText().toString();
                 String audioFilePath = audioRecorder.getFileName();
                 Post post = new Post(audioFilePath, caption);
+
+                Ion.with(v.getContext())
+                .load("https://koush.clockworkmod.com/test/echo")
+                        .setJsonPojoBody(post)
+                        .asJsonObject()
+                        .setCallback(new FutureCallback<JsonObject>() {
+                            @Override
+                            public void onCompleted(Exception e, JsonObject result) {
+                                if (e != null) {
+                                    Log.e(LOG_TAG, e.toString());
+                                }
+                                Log.d(LOG_TAG, result.toString());
+                            }
+                        });
                 Toast.makeText(v.getContext(), "This will post this message one day.", Toast.LENGTH_SHORT).show();
             }
         };
